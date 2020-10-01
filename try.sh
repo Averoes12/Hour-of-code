2e54d17b45cb397329b0348a67957036b70262d3
Dhia Ul Haq <qahludhia@gmail.com>
2020-10-01 19:53:29 +0700

    Ganti bahasa

--- try.sh
+++ try.sh
@@ -2,29 +2,29 @@
 
 MENIT=`date +%M`
 JAM=`date +%H`
-echo "what's your name ?"
+echo "what's your name?"
 read name 
-echo "salam kenal" $name
+echo "Nice to meet You" $name
 
 
-echo "Silahkan Masukkan password :"
+echo "Please Insert Your Password:"
 read password
 
 if [ "$password" == "daff" ];
 then
- echo "password benar"
- echo "anda telah login pada $JAM : $MENIT"
+ echo "Correct Password"
+ echo "You Are Logged in $JAM : $MENIT"
 else
- echo "password salah"
- echo "silahan coba lagi"
+ echo "Incorrect Password"
+ echo "Please Try Again"
 fi
 
- echo "Silahkan pilih menu anda"
+ echo "Choose Your Menu"
 for menu in sate=25k bakso=10k ayam=20k mie=10k;
 do
   echo "$menu";
 done
- echo "Silahkan pilih "
+ echo "Please Choose"
  read makanan
 
 case $makanan in
@@ -42,7 +42,7 @@ for gorengan in bakwan risol tahu tempe tahu_pedes bakwan_jagung
 do 
  echo "$gorengan"
 done
- echo "Pilih gorengan"
+ echo "Choose gorengan"
  read pilihan
 
 case $gorengan in
@@ -53,5 +53,5 @@ tahu_pedes|bakwan_jagung) echo "$pilihan 1000"
 ;;
 esac
 
-echo "Selamat menikmati"
-echo "Harap bayar dengan uang pas"
+echo "Have a nice meal"
+echo "Pay with e-money"
